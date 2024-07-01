337f7df2f2067c1f0d1b901f06ada24c5be3b44c
Eduardo Sewnarine Carrilho <eduardo.carrilhoest@gmail.com>
2024-07-01 16:17:23 -0300

    JS

--- exercícios/Clima/sw.js
+++ exercícios/Clima/sw.js
@@ -41,7 +41,7 @@ async function registerPeriodicNewsCheck() {
   const registration = await navigator.serviceWorker.ready;
   try {
     await registration.periodicSync.register("get-latest-news", {
-      minInterval: 24 * 60 * 60 * 1000,
+      minInterval: 1 * 60 * 1000,
     });
   } catch {
     console.log("Periodic Sync could not be registered!");
